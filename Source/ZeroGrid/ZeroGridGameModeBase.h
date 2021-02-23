eeb7252bb502eedbd373eac1bf09dcccce55f15f
GOMES PEREIRA João Miguel <joaomiguel.gomespereira@altran.com>
2021-02-23 20:47:44 +0000

    Initial Project Setup

---
+++ Source/ZeroGrid/ZeroGridGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ZeroGridGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZEROGRID_API AZeroGridGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
